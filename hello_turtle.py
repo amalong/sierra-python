b900fb70c31bf83064d61619d28c1f4dc42e334e
Jared Amalong <amalong@gmail.com>
2019-08-30 14:39:19 -0700

    added have fun comment

--- hello_turtle.py
+++ hello_turtle.py
@@ -10,3 +10,5 @@ my_turtle.forward(50)         # move forward 50 without drawing
 my_turtle.right(90)           # change direction to the right, left works too
 my_turtle.down()              # put the tail down
 my_turtle.backward(100)       # draw a green line 100 units long
+
+#have fun!
